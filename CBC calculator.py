26dd5c490c3dac5b92f7744141bf828a11b02e00
AbigailAbrahams <abrahamsa1114@students.bowiestate.edu>
2023-05-03 18:08:46 -0400

    Draft 1

---
+++ CBC calculator.py
@@ -0,0 +1,16 @@
+def request():
+    gender = input("Enter Gender:")
+    red_blood_cells = input("Enter Red Blood Cell Count:")
+
+
+
+
+# def determine(red_blood_count):
+#     if (str(gender))
+#     print(determine(red_blood_count))
+
+
+
+
+
+request()

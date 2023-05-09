9dc73987244d50a2656f497827b38148bd890738
OGOCHUKWUHALLIDAY <ogochukwu.halliday@gmail.com>
2023-05-08 21:39:51 -0400

    Group assign

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+def request():
+    gender = input("Enter Gender:")
+    rbc_count = input("Enter Red Blood Cell Count:")
+    (RBC(rbc_count, gender))
+    wbc_count = input("Enter White Blood Cell Count:")
+    plt_count = input("Enter Platelets Count:")
+    hct_count = input("Enter Hematocrit Count:")
+    hgb_count = input("Enter Hemoglobin Count:")
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def RBC(rbc_count, gender):
+    if (str (gender) == "male" and float(rbc_count)>=4.2 and float(rbc_count)<=6.0):
+        print ("RBC Count= Normal")
+    elif(str(gender)== "male" and float(rbc_count)<4.2):
+        print("RBC Count= Low/Anemia")
+    elif(str(gender)== "male" and float(rbc_count)>6.0):
+        print ("RBC Count= High/Polycythemia")
+    elif (str (gender) == "female" and float(rbc_count)>=3.8 and float(rbc_count)<=5.2):
+        print ("RBC Count= Normal")
+    elif(str(gender)== "female" and float(rbc_count)<3.8):
+        print("RBC Count= Low/Anemia")
+    elif(str(gender)== "female" and float(rbc_count)>5.2):
+        print ("RBC Count= High/Polycythemia")
+
+request()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi Friend12345, {name }')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

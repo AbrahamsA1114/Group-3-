a3a64b9e4a2f96d0ee8e7e6b6af0432465d9614b
felic <felicia.a.ilori@gmial.com>
2023-05-14 23:04:33 -0400

    GROUP PROJECT

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def request():
     plt_count = input("Enter Platelets Count:")
     (PLT(plt_count, gender))
     hct_count = input("Enter Hematocrit Count:")
+    (HCT(hct_count, gender))
     hgb_count = input("Enter Hemoglobin Count:")
+    (HGB(hgb_count, gender))
 
 
 def RBC(rbc_count, gender):
@@ -51,6 +53,35 @@ def PLT (plt_count , gender):
         print("PLT Count = High/Thrombocytosis")
     elif (str(gender)== "female" and float(plt_count)<150):
         print("PLT Count = Low/Thrombocytopenia")
+
+def HCT(hct_count, gender):
+    if(str(gender)=="male" and float(hct_count)>=0.40 and float(hct_count)<=0.54):
+        print("HCT Count = Normal")
+    elif(str(gender)=="male" and float(hct_count)<0.40):
+        print("HCT Count = Low/Anemia")
+    elif(str(gender)=="male" and float(hct_count)>0.54):
+        print("HCT Count = High/Polycythemia")
+    elif(str(gender)=="female" and float(hct_count)>=0.35 and float(hct_count)<=0.49):
+        print("HCT Count = Normal")
+    elif(str(gender)=="female" and float(hct_count)<0.35):
+        print("HCT Count = Low/Anemia")
+    elif(str(gender)=="female" and float(hct_count)>0.49):
+        print("HCT Count = High?polycythemia")
+
+def HGB(hgb_count, gender):
+    if(str(gender)=="male" and float(hgb_count)>=135 and float(hgb_count)<=180):
+        print("HGB Count = Normal")
+    elif(str(gender)=="male" and float(hgb_count)<135):
+        print("HGB Count = Low/Anemia")
+    elif(str(gender)=="male" and float(hgb_count)>180):
+        print("HGB Count = High/Polycythemia")
+    elif(str(gender)=="female" and float(hgb_count)>=120 and float(hgb_count)<=150):
+        print("HGB Count = Normal")
+    elif(str(gender)=="female" and float(hgb_count)<120):
+        print("HGB Count = Low/Anemia")
+    elif(str(gender)=="female" and float(hgb_count)>150):
+        print("HGB Count = High/Polycythemia")
+
 request()
 
 
